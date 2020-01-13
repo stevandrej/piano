fa1a777f8989c41545bb896a92b29510c90bb949
Andrej Stevkovski <stevandrej@yahoo.com>
2020-01-13 01:29:09 +0100

    Updates

---
+++ precache-manifest.200815d3c47d4c2bf1a1457105f24f2d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872529f8db4201f9b3d9c597bc8156bf",
+    "url": "/stevandrej/piano/index.html"
+  },
+  {
+    "revision": "ed608d2ae0e47b46c44d",
+    "url": "/stevandrej/piano/static/css/main.2f4e505c.chunk.css"
+  },
+  {
+    "revision": "d22a5a87d79020bf5c1c",
+    "url": "/stevandrej/piano/static/js/2.5ad7edbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stevandrej/piano/static/js/2.5ad7edbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed608d2ae0e47b46c44d",
+    "url": "/stevandrej/piano/static/js/main.8be82fa2.chunk.js"
+  },
+  {
+    "revision": "dbea356d6d5fbda4e375",
+    "url": "/stevandrej/piano/static/js/runtime-main.aa3f6df7.js"
+  },
+  {
+    "revision": "0c0eef9c56140ef00dcdab0cf2707100",
+    "url": "/stevandrej/piano/static/media/do-high.0c0eef9c.mp3"
+  },
+  {
+    "revision": "ffb2d0de5a24cba41848441097efd05a",
+    "url": "/stevandrej/piano/static/media/do.ffb2d0de.mp3"
+  },
+  {
+    "revision": "3e2fb0e22c3a427f00e7cbc12e2a9462",
+    "url": "/stevandrej/piano/static/media/fa.3e2fb0e2.mp3"
+  },
+  {
+    "revision": "0d4606d0780ca269780418579c15d28d",
+    "url": "/stevandrej/piano/static/media/la.0d4606d0.mp3"
+  },
+  {
+    "revision": "3ab731f506ed87966063ea1c91a8b097",
+    "url": "/stevandrej/piano/static/media/mi.3ab731f5.mp3"
+  },
+  {
+    "revision": "0f8445aa73d3649b47866f91ded5843a",
+    "url": "/stevandrej/piano/static/media/re.0f8445aa.mp3"
+  },
+  {
+    "revision": "afab7370aeaf9bd8c182c56329c92633",
+    "url": "/stevandrej/piano/static/media/si.afab7370.mp3"
+  },
+  {
+    "revision": "1a5ea7eb8d1e27138129e7bc6151034c",
+    "url": "/stevandrej/piano/static/media/sol.1a5ea7eb.mp3"
+  }
+]);
