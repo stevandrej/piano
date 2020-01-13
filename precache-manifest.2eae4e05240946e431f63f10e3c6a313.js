451603c6aff6b295b87ee25462d8aba673435968
Andrej Stevkovski <stevandrej@yahoo.com>
2020-01-13 01:32:03 +0100

    Updates

---
+++ precache-manifest.2eae4e05240946e431f63f10e3c6a313.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3debf38659696afd4dc7c8ed2a923bc0",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "ed608d2ae0e47b46c44d",
+    "url": "/piano/static/css/main.2f4e505c.chunk.css"
+  },
+  {
+    "revision": "d22a5a87d79020bf5c1c",
+    "url": "/piano/static/js/2.5ad7edbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/piano/static/js/2.5ad7edbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed608d2ae0e47b46c44d",
+    "url": "/piano/static/js/main.8be82fa2.chunk.js"
+  },
+  {
+    "revision": "1664da884654c74b3356",
+    "url": "/piano/static/js/runtime-main.959c6872.js"
+  },
+  {
+    "revision": "0c0eef9c56140ef00dcdab0cf2707100",
+    "url": "/piano/static/media/do-high.0c0eef9c.mp3"
+  },
+  {
+    "revision": "ffb2d0de5a24cba41848441097efd05a",
+    "url": "/piano/static/media/do.ffb2d0de.mp3"
+  },
+  {
+    "revision": "3e2fb0e22c3a427f00e7cbc12e2a9462",
+    "url": "/piano/static/media/fa.3e2fb0e2.mp3"
+  },
+  {
+    "revision": "0d4606d0780ca269780418579c15d28d",
+    "url": "/piano/static/media/la.0d4606d0.mp3"
+  },
+  {
+    "revision": "3ab731f506ed87966063ea1c91a8b097",
+    "url": "/piano/static/media/mi.3ab731f5.mp3"
+  },
+  {
+    "revision": "0f8445aa73d3649b47866f91ded5843a",
+    "url": "/piano/static/media/re.0f8445aa.mp3"
+  },
+  {
+    "revision": "afab7370aeaf9bd8c182c56329c92633",
+    "url": "/piano/static/media/si.afab7370.mp3"
+  },
+  {
+    "revision": "1a5ea7eb8d1e27138129e7bc6151034c",
+    "url": "/piano/static/media/sol.1a5ea7eb.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stevandrej/piano/precache-manifest.200815d3c47d4c2bf1a1457105f24f2d.js"
+  "/piano/precache-manifest.2eae4e05240946e431f63f10e3c6a313.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stevandrej/piano/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/piano/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
